0eb015c33ebc319ae319fb462dde92137af6b0fc
Yeshenko, Dmytro <dmytro.yeshenko@juliusbaer.com>
2024-01-22 20:10:05 +0100

    Temporary disable routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Drools from "./component/drools/Drools"
 import Second from "./component/Second"
 import Home from "./component/Home"
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/drools" element={<Drools/>} />
-        <Route path="/second" element={<Second/>} />
-      </Routes>
-  </Router>
+    <Drools />
+  //   <Router>
+  //     <Routes>
+  //       <Route path="/" element={<Home/>} />
+  //       <Route path="/drools" element={<Drools/>} />
+  //       <Route path="/second" element={<Second/>} />
+  //     </Routes>
+  // </Router>
   );
 }
 
