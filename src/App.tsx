b4465f969016fc67151a33a4db52b0ed101a29f2
Yeshenko, Dmytro <dmytro.yeshenko@juliusbaer.com>
2024-04-15 16:41:31 +0200

    hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
 import ProcessForm from './components/ProcessForm';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import DisplayJsonFormDialog from './components/form/DisplayJsonFormDialog';
 import HelloComponent from './components/hello/HelloComponent';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/builder" element={<ProcessForm />} />
         <Route path="/builder/form" element={<DisplayJsonFormDialog />} />
         <Route path="/" element={<HelloComponent />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
